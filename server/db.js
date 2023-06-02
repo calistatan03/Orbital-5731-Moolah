1e8b605ba35648582fbe830f6c4ee5bd2407e782
Calista <calistatan03@gmail.com>
2023-06-02 23:50:46 +0800

    update again.

---
+++ server/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connection = () => {
+  mongoose.connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Database connection error:', error);
+  });
+};
+
+module.exports = connection;
