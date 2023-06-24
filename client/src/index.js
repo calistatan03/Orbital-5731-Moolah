24a1846f4e75853b59070ab7c46125d26e000e5b
Petrine Pang <petrinepang09@gmail.com>
2023-06-24 15:37:39 +0800

    update 24/06

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 ReactDOM.render(
   <React.StrictMode>
